e33bb3d0b7ab1280f09c79f93896256ab1451a96
karmash <prof-assistant.yandex.ru>
2020-06-16 16:49:47 +0800

    Переделал компонент Loader. Чтобы в нем не было 6 подряд одинаковых div-элементов. Сделал через массив

--- src/components/App/Loader/index.tsx
+++ src/components/App/Loader/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './index.sass';
 
-const Loader: React.FC = () => (
-  <div className="container_loader">
-    <div className="sk-chase">
-      <div className="sk-chase-dot" />
-      <div className="sk-chase-dot" />
-      <div className="sk-chase-dot" />
-      <div className="sk-chase-dot" />
-      <div className="sk-chase-dot" />
-      <div className="sk-chase-dot" />
-    </div>
-    <div className="text_loader">Идёт загрузка данных</div>
-  </div>
+const Loader: React.FC = () => {
+  const arrayCount = [1, 2, 3, 4, 5, 6];
 
-);
+  return (
+    <div className="container_loader">
+      <div className="sk-chase">
+        {
+            arrayCount.map((elem) => <div key={elem} className="sk-chase-dot" />)
+        }
+      </div>
+      <div className="text_loader">Идёт загрузка данных</div>
+    </div>
+  );
+};
 
 export default Loader;
